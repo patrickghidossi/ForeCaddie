5538af08ccf6b73836ef582ce910526448468745
patrickghidossi <patrick.ghidossi@gmail.com>
2016-09-01 16:33:33 -0600

    Server Update

--- forecaddie_server/server.js
+++ forecaddie_server/server.js
@@ -10,7 +10,7 @@ var GameModel = require("./GolfSchema")(mongoose);
 
 var app = express();
 
-var PORT = process.env.port || 8000;
+var PORT = process.env.port || 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
